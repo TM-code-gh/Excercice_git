e9627c9ec153e5d952d41855640e0537a865cf41
TM-code-gh <111370515+TM-code-gh@users.noreply.github.com>
2024-01-26 17:24:32 +0100

    Compare student by mean

--- first.py
+++ first.py
@@ -16,9 +16,20 @@ def create(values):
 show()
 create(['A','B','C','C*','D','E','F','F*','F**','G','H'])
 
-def moyenne(values):
+def mean(values):
     return sum(values)/len(values)
 
+student_A_mean = mean([20, 15, 12, 10])
+student_B_mean = mean([12, 12, 12, 15])
 
-print(moyenne([20, 15, 12, 10]))
-print(moyenne([12, 12, 12, 15]))
+students_mean=[student_A_mean, student_B_mean]
+
+def get_best_student(students_mean):
+    best = students_mean[0]
+    for student_mean in students_mean[1:]:
+        if student_mean>best:
+            best = student_mean
+    return students_mean.index(best)
+            
+        
+    
